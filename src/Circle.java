3195600c2a6f7006ffbfa28374acd084278880c3
Ricardo <ricardoharo@outlook.com>
2016-10-21 20:54:35 -0500

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Ricardo on 10/21/16.
+ */
+public class Circle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/Shape.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Ricardo on 10/21/16.
+ */
+public interface Shape {
+    void draw();
+}
